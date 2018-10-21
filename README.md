2eba15d3bd80eb84f48f46ac80279979b9b30536
Esther Leytush <esther.leytush@betterment.com>
2018-10-21 00:29:47 -0400

    add tallying concept

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 ## Harry Potter Jeopardy
 
-25 questions, 3 of which are secret double pointers!
+30 questions, 3 of which are secret double pointers!
 
-Before players start: look into each other's eyes, and say "I swear by my pretty floral bonnet that I will end you"
-
-![firefly-image](/assets/game-screenshot.jpg)
+![firefly-image](/assets/images/screenshot.png)
 
 ### To play
 
-Cast the [index page](http://mindplace.github.io/potter-jeopardy/) to a large screen. Click cards to have them flip.
+Cast the [index page](http://mindplace.github.io/potter-jeopardy/) to a large screen. Click cards to have them flip. Click again for answers.
 
 The double pointers are bordered in red.
 
-To access answers, open the same [index page](http://mindplace.github.io/potter-jeopardy/) on your phone. On mobile view, the cards show their answers instead of their questions.
+#### Gameplay
+
+Break apart into house teams!
+
+Taking turns, each team gets to choose a question. If they answer correctly, they get the points.  
+If they can't answer it correctly, each other team gets to try answering it. Whoever gets the answer gets the points. Add the points to the tally.
+
+When all questions have been answered, the team with the most points wins!
 
 #### Our Prizes
-1.
-2.
-3.
-4.
+1. 50 house points, and stickers
+2. 30 house points, and Chocolate Frogs
+3. 10 house points, and Golden Ferro-Rocher Snitches
+4. Consoling Hugs
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,13 +10,23 @@ function hideAnswer(card){
   }, 300);
 };
 
+function calculateHousePoints(){
+  ['Gryffindor', 'Hufflepuff', 'Slytherin', 'Ravenclaw'].forEach(function(elem){
+    var cards = $('.' + elem + '-point');
+    var cardPointsArray = $.map(cards, function(item){ return parseInt($(item).find('.front .inner-text').text()); });
+    var totalPoints = cardPointsArray.reduce(function(reducer, val) { return reducer + val }, 0);
+    var counter = $('#' + elem.toLowerCase() + '-counter');
+    $(counter).text(totalPoints);
+  })
+};
+
 $(document).ready(function(e){
 
-  $(".card").flip();
+  $('.card').flip();
 
-  $(".card .back").on("click", function(e){
+  $('.card .back').on('click', function(e){
     var card = $(this).parent().first();
-    var questionIsVisible = $(card).find(".question").is(':visible');
+    var questionIsVisible = $(card).find('.question').is(':visible');
 
     if (questionIsVisible) {
       showAnswer(card);
@@ -26,5 +36,29 @@ $(document).ready(function(e){
       hideAnswer(card);
     }
   });
+
+  $('.card').on('click', function(e){
+    $('#current-card').removeAttr('id');
+    $(this).attr('id', 'current-card');
+  });
+
+  $('.house').on('click', function(e){
+    if ($('#current-card').length) {
+      var houseName = $(this).find('h1').text();
+      var unclaimed = !$('#current-card').find('.house-name').length;
+      var beingAnswered = $('#current-card').find('.question').is(':visible') || $('#current-card').find('.answer').is(':visible');
+
+      if (unclaimed && beingAnswered) {
+        var newElement = '<div class="house-name red">' + houseName + '</div>';
+        $('#current-card').find('.front').append(newElement);
+        $('#current-card').find('.answer').append(newElement);
+        $('#current-card').find('.question').append(newElement);
+        $('#current-card').addClass(houseName + '-point');
+        $('#current-card').removeAttr('id');
+      }
+      
+      calculateHousePoints();
+    }
+  })
 
 });
