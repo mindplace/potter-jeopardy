695a08da8e5b4d2e3b858734808910eab44392b4
Esther Leytush <esther.leytush@betterment.com>
2018-10-20 22:57:02 -0400

    various updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,30 @@
+function showAnswer(card){
+  $(card).find('.question').hide();
+  $(card).find('.answer').fadeIn('slow');
+};
+
+function hideAnswer(card){
+  setTimeout(function(){
+    $(card).find('.answer').hide();
+    $(card).find('.question').show();
+  }, 300);
+};
+
 $(document).ready(function(e){
 
   $(".card").flip();
+
+  $(".card .back").on("click", function(e){
+    var card = $(this).parent().first();
+    var questionIsVisible = $(card).find(".question").is(':visible');
+
+    if (questionIsVisible) {
+      showAnswer(card);
+      e.preventDefault();
+      e.stopImmediatePropagation();
+    } else {
+      hideAnswer(card);
+    }
+  });
 
 });
